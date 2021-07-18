18511a69c901351d31413a5bd1ffad5e6df7e92e
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-07-18 13:19:09 -0300

    Descending Order - 7 kyu

---
+++ descendingOrder.js
@@ -0,0 +1,17 @@
+/* 
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321 
+*/
+// First convert the number in a String so you can use the arrays methods inside of it. After that, you can use split to put all values inside a array.
+// sort for sorting the numbers  and joint and - 0 to join all the array and transform in a number again.
+
+const descendingOrder = n => n.toString().split('').sort((a, b) => b - a).join('') - 0;
+
+
+console.log(descendingOrder(1201));
